21014b1238ff1273d59e890550745642427585f8
123-kartik <kartikmpaliwal@gmail.com>
2022-03-28 16:13:58 +0530

    new

---
+++ loopassignment/CubeBt1to5.java
@@ -0,0 +1,17 @@
+package loopassignment;
+
+public class CubeBt1to5 {
+      void display()
+      {
+    	  for(int i=1;i<=5;i++)
+    	  {
+    		  System.out.println(i*i*i);
+    	  }
+      }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CubeBt1to5 Cu=new CubeBt1to5();
+		Cu.display();
+	}
+
+}
